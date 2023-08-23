de84f533bf337af21ad914ae422e84cb61d48009
aanchan <aanchan@synaptitude.info>
2023-08-22 17:15:28 -0700

    updated training_script to work with updated data

--- training_script.py
+++ training_script.py
@@ -4,8 +4,28 @@ from datasets import load_dataset, Audio
 import json
 from transformers import Wav2Vec2CTCTokenizer, Wav2Vec2FeatureExtractor, Wav2Vec2Processor
 
-def main(input_dir: str, output_dir: str):
+file_path_column = "filename_old"
 
+
+def change_file_paths(data_instance, data_dir: str):
+    data_instance[file_path_column] = os.path.join(data_dir,
+                                                   data_instance[file_path_column])
+    return data_instance
+
+def prepare_dataset(data_instance, processor: Wav2Vec2Processor):
+    audio = data_instance[file_path_column]
+
+    data_instance["input_values"] = processor(audio["array"],
+                                              sampling_rate=audio["sampling_rate"])
+
+    data_instance["input_length"] = len(data_instance["input_values"])
+    with processor.as_target_processor():
+        data_instance["labels"] = processor(data_instance["transcript"]).input_ids
+
+    return data_instance
+
+
+def main(input_dir: str, output_dir: str):
     dataset_dict = load_dataset('csv', data_files={
         "train": '/home/data1/psst-data-csv/train_utterances_excel.csv',
         "valid": '/home/data1/psst-data-csv/valid_utterances_excel.csv',
@@ -19,8 +39,8 @@ def main(input_dir: str, output_dir: str):
         dict_ltr[arpa] = idx
 
     vocab_file = os.path.join(output_dir, 'psst_dict.json')
-    with open(vocab_file) as vocab_file:
-        json.dump(dict_ltr, vocab_file)
+    with open(vocab_file, mode="w") as vocab_file_json:
+        json.dump(dict_ltr, vocab_file_json)
 
     tokenizer = Wav2Vec2CTCTokenizer(vocab_file,
                                      unk_token=psstdata.UNK,
@@ -36,16 +56,21 @@ def main(input_dir: str, output_dir: str):
     processor = Wav2Vec2Processor(feature_extractor=feature_extractor,
                                   tokenizer=tokenizer)
 
-    train_dataset = dataset_dict["train"].cast_column("filename_new",
-                                                      Audio(sampling_rate=16000))
-    valid_dataset = dataset_dict["valid"].cast_column("filename_new",
-                                                      Audio(sampling_rate=16000))
-    test
+    for dataset in ("train", "valid", "test"):
+        data_dir = os.path.join(data_input_dir, dataset)
+        dataset_dict[dataset] = dataset_dict[dataset].map(change_file_paths,
+                                                          fn_kwargs={"data_dir": data_dir})
+        dataset_dict[dataset] = dataset_dict[dataset].cast_column(file_path_column,
+                                                                  Audio(sampling_rate=16000))
+        dataset_dict[dataset] = dataset_dict[dataset].map(prepare_dataset,
+                                                          remove_columns=dataset_dict[dataset].column_names,
+                                                          num_proc=4,
+                                                          fn_kwargs={"processor": processor})
 
-
+    print("hellos")
 
 
 if __name__ == "__main__":
-    data_input_dir = "/home/data1/psst-data"
+    data_input_dir = "/home/data1/psst-data/psst-data-2022-03-02-full"
     data_output_dir = "/home/data1/psst-data-out"
     main(data_input_dir, data_output_dir)
