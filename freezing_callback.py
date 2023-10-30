516cc5b233c92e45194214a39d90f79631469010
laurynflu <98038908+laurynflu@users.noreply.github.com>
2023-10-30 11:17:14 -0400

    Update freezing_callback

--- freezing_callback.py
+++ freezing_callback.py
@@ -3,26 +3,28 @@ import re
 import math
 
 class FreezingCallback(TrainerCallback):
-    def __init__(self, freezing_schedule: list, trainer: Trainer, model_config: dict):
+    def __init__(self, freezing_schedule: list, trainer: Trainer, model_config: dict, num_hidden_layers: int):
         self.model_config = model_config
         self.trainer = trainer
         self.freezing_schedule = freezing_schedule
         self.current_step_idx = 0
         self.model = trainer.model
+        self.num_hidden_layers = num_hidden_layers
+
 
     def on_epoch_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         # Check if current epoch is greater than or equal to the target epoch 
         if state.epoch >= self.freezing_schedule[self.current_step_idx][1]:
             self.current_step_idx += 1
             # Called to unfreeze layers
-            self.freeze_model(self.freezing_schedule[self.current_step_idx][0], self.model_config["n_layer"], int(state.epoch))
+            self.freeze_model(self.freezing_schedule[self.current_step_idx][0], int(state.epoch))
 
     def on_save(self, args, state, control, **kwargs):
         for name, param in self.trainer.model.named_parameters():
             param.requires_grad = True
 
-    def freeze_model(self, freeze_to, highest_layer, epoch):
-        print(f"\nEpoch {epoch}: Freezing model to layer {freeze_to} of {highest_layer} layers.")
+    def freeze_model(self, unfreeze_layers, epoch):
+        print(f"\nEpoch {epoch}: Unfreezing layers from {unfreeze_layers} to {self.num_hidden_layers}.")
 
         for name, param in self.trainer.model.named_parameters():
             # Find out the number of every layer
@@ -30,6 +32,8 @@ class FreezingCallback(TrainerCallback):
                 layer_number = int(re.search(r'\.h\.\d+\.', name).group().strip(".h"))
             except AttributeError:
                 layer_number = math.inf
-            # Freeze all layers up to layer `freeze_to`, including embedding layers.
-            if '.wte.' in name or '.wpe.' in name or layer_number <= freeze_to:
+            # Unfreeze layers from `unfreeze_layers` to `self.num_hidden_layers`.
+            if '.wte.' in name or '.wpe.' in name or (6 <= layer_number <= self.num_hidden_layers and layer_number >= unfreeze_layers):
+                param.requires_grad = True
+            else:
                 param.requires_grad = False
